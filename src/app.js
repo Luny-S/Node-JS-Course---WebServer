cf73cdc84fadd8d81b04a0b221901d9091cad451
Aleksander Sil <olek130@gmail.com>
2019-12-25 22:29:11 +0100

    Created JSON endpoint for weather forecast

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const geocode = require('../utils/geocode');
+const forecast = require('../utils/forecast');
 
 const app = express();
 
@@ -16,44 +18,79 @@ const partialsPath = path.join(__dirname, '../templates/partials');
  **/
 
 // Setup handlebars engine and views location
-app.set('view engine','hbs');
-app.set('views',viewPath);
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
 hbs.registerPartials(partialsPath);//
 
 // Setup static directory to serve
 app.use(express.static(assetsPath)); // customize server
 
 
-
-
-app.get('',(req,res) => {
+app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
         name: 'Aleksander S.'
     });
 });
 
-app.get('/about', (req,res)=>{
-    res.render('about',{
+app.get('/about', (req, res) => {
+    res.render('about', {
         title: "About me",
         content: "I'm learning some NodeJS"
     });
 });
 
-app.get('/help', (req,res)=>{
-    res.render('help',{
+app.get('/help', (req, res) => {
+    res.render('help', {
         title: "Help",
         content: "Help page content should go here"
     });
 });
 
-app.get('/help/*', (req,res) => {
-    res.render('404',{
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address.'
+        });
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+        if (error) {
+            return res.send({error});
+        }
+
+        forecast(longitude, latitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error});
+            }
+            res.send({
+                'forecast': forecastData,
+                location,
+                'address': req.query.address
+            });
+        });
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        });
+    }
+    console.log(req.query);
+    res.send({
+        products: []
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
         errorMessage: "Help page for this article does not exist"
     });
 });
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
     res.render('404', {
         errorMessage: "Page not found"
     });
